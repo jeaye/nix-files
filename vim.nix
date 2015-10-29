c4b8d9b7acb305078ed96e9d05a3800227d816cc
jeaye <contact@jeaye.com>
2015-10-29 21:13:41 +0800

    Update color_coded version

--- vim.nix
+++ vim.nix
@@ -4,14 +4,6 @@
   environment.systemPackages = with pkgs; [
     vimHugeX
     vimPlugins.YouCompleteMe
-    /nix/store/8fd65w5565kvp3kz1cfqv5lh2x0gj4h7-color_coded
+    /nix/store/rkda5lij6wpw687aa3w8hc9bz6kqlhpb-color_coded
   ];
-
-  environment.etc.vimrc = {
-    text = ''
-      set rtp+=${pkgs.color_coded}
-      source /home/jeaye/projects/vimrc/vimrc
-    '';
-  };
-  environment.systemPackages = [ pkgs.vim ];
 }
