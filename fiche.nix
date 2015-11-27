52a5640bcc108a8c01274a4c6936d8beb6d771b4
jeaye <contact@jeaye.com>
2015-11-27 20:17:09 +0800

    fiche

--- fiche.nix
+++ fiche.nix
@@ -7,7 +7,7 @@
         owner = "solusipse";
         repo = "fiche";
         rev = "8f3e23d3de66aa894f78417523cb6aeb208b6d60";
-        sha256 = "1fhdp90k4dv5vps3fzip0g64haf8j6vp2mpk17w89qg7kyb1bwi6";
+        sha256 = "1371gafnysddg7zip2rdrp3wd8n4xc6ddvkcqfg4b4jc6i2hxxax";
       };
       target = "fiche";
     }
