cd50893f6a938d6567c5665dd4b0ab49fc4ef312
jeaye <contact@jeaye.com>
2015-11-27 22:30:01 +0800

    Tighten up on httpd access

--- httpd.nix
+++ httpd.nix
@@ -16,15 +16,17 @@
         hostName = "paste.jeaye.com";
         serverAliases = ["pastespace.org"];
         documentRoot = "/home/http/paste.jeaye.com";
-        extraConfig = ''
-        <Directory /home/http/paste.jeaye.com>
-          AllowOverride All
-          DirectoryIndex index.html index.txt
-          Require all granted
-        </Directory>
-        '';
       }
     ];
+
+    extraConfig = ''
+    <Directory /home/http/paste.jeaye.com>
+      AllowOverride All
+      DirectoryIndex index.html index.txt
+      Require all granted
+      Options -Indexes
+    </Directory>
+    '';
   };
 
   users.extraUsers.http = {
