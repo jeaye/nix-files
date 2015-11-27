af639c64642d4c3841f6036cbf734c9b8449cf17
jeaye <contact@jeaye.com>
2015-11-27 19:51:08 +0800

    Use a different syntax for fetchFromGithub

--- fiche.nix
+++ fiche.nix
@@ -1,5 +1,6 @@
-{ config, pkgs, fetchFromGitHub, ... }:
+{ config, pkgs, ... }:
 
+{ fetchFromGitHub }:
 {
   environment.etc.fiche = fetchFromGitHub {
     owner = "solusipse";
@@ -7,7 +8,9 @@
     rev = "8f3e23d3de66aa894f78417523cb6aeb208b6d60";
     sha256 = "1fhdp90k4dv5vps3fzip0g64haf8j6vp2mpk17w89qg7kyb1bwi6";
   };
+}
 
+{
   #systemd.services.fiche = {
   #  wantedBy = [ "multi-user.target" ];
   #  after = [ "network.target" ];
