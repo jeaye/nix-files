c6646fffa3e66e63019573aa1f1918b26f3d6604
jeaye <contact@jeaye.com>
2015-11-28 00:11:30 +0800

    Add nproc limits

--- user.nix
+++ user.nix
@@ -2,6 +2,10 @@
 
 {
   security.sudo.wheelNeedsPassword = true;
+  security.pam.loginLimits = [
+    { domain = "*"; item = "nproc"; type = "soft"; value = "500"; }
+    { domain = "*"; item = "nproc"; type = "hard"; value = "500"; }
+  ];
 
   users.extraUsers.jeaye = {
     isNormalUser = true;
