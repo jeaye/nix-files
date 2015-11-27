f7a33a1bc1266c5ac312a079609b59753032286a
jeaye <contact@jeaye.com>
2015-11-27 19:22:49 +0800

    Remove irc service

--- irc.nix
+++ irc.nix
@@ -7,21 +7,6 @@
     mutt
   ];
 
-  systemd.services.ircSession = {
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
-    serviceConfig = {
-      Type = "forking";
-      User = "irc";
-      ExecStart = ''
-        ${pkgs.tmux}/bin/tmux new-session -d -s irc ${pkgs.weechat}/bin/weechat && ${pkgs.tmux}/bin/tmux new-window -t irc:1 ${pkgs.mutt}/bin/mutt
-      '';
-      ExecStop = ''
-        ${pkgs.tmux}/bin/tmux kill-session -t irc
-      '';
-    };
-  };
-
   users.extraUsers.irc = {
     isNormalUser = true;
     home = "/home/irc";
