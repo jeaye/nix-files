d985d55d13550ce5274fea28e5720829f96f4fcf
jeaye <contact@jeaye.com>
2015-11-28 16:21:07 +0800

    Fix some indenting

--- postfix.nix
+++ postfix.nix
@@ -68,14 +68,14 @@
     '';
     extraMasterConf = ''
       submission inet n       -       n       -       -       smtpd
-      -o syslog_name=postfix/submission
-      -o smtpd_tls_wrappermode=no
-      -o smtpd_tls_security_level=encrypt
-      -o smtpd_sasl_auth_enable=yes
-      -o smtpd_recipient_restrictions=permit_mynetworks,permit_sasl_authenticated,reject
-      -o milter_macro_daemon_name=ORIGINATING
-      -o smtpd_sasl_type=dovecot
-      -o smtpd_sasl_path=private/auth
+        -o syslog_name=postfix/submission
+        -o smtpd_tls_wrappermode=no
+        -o smtpd_tls_security_level=encrypt
+        -o smtpd_sasl_auth_enable=yes
+        -o smtpd_recipient_restrictions=permit_mynetworks,permit_sasl_authenticated,reject
+        -o milter_macro_daemon_name=ORIGINATING
+        -o smtpd_sasl_type=dovecot
+        -o smtpd_sasl_path=private/auth
     '';
   };
 }
