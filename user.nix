c4691ce432a66f0fb7d4833533d6fb052ebece6d
jeaye <contact@jeaye.com>
2015-11-28 23:18:46 +0800

    Enable mutable users for now

--- user.nix
+++ user.nix
@@ -16,10 +16,9 @@
     isNormalUser = true;
     home = "/home/jeaye";
     createHome = true;
-    hashedPassword = "a860c9cae665b4d3240d1518cd55406cdf3a15a0d356aa965af1cf74fab62ed33f41ce112935bd34bb8ba047f1d3b5337f0c7e775dc8ad942b7995ffcf58b0d3";
     extraGroups = [ "wheel" ];
   };
 
   # Can't use useradd/groupadd imperatively
-  users.mutableUsers = false;
+  #users.mutableUsers = false;
 }
