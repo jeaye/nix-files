8fc4da7278afd167c323e699ca02cc8bd532d210
jeaye <contact@jeaye.com>
2015-11-28 17:48:40 +0800

    Force tls auth with postfix

--- postfix.nix
+++ postfix.nix
@@ -54,10 +54,11 @@
       smtpd_tls_session_cache_database = btree:''${data_directory}/smtpd_scache
       smtp_tls_session_cache_database = btree:''${data_directory}/smtp_scache
       smtpd_tls_wrappermode = no
-      smtpd_tls_security_level = encrypt
+      smtpd_tls_security_level = may
       smtpd_tls_protocols = !SSLv2, !SSLv3
 
       smtpd_sasl_auth_enable = no
+      smtpd_tls_auth_only = yes
       smtpd_sasl_type = dovecot
       smtpd_sasl_path = private/auth
       smtpd_sasl_authenticated_header = yes
