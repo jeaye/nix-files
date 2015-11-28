b08880828b571b9919175c66346f11e61022f6f6
jeaye <contact@jeaye.com>
2015-11-28 15:53:24 +0800

    Expand dovecot

--- dovecot.nix
+++ dovecot.nix
@@ -6,10 +6,34 @@
     enableImap = true;
     enablePop3 = false;
     mailLocation = "maildir:~/Maildir";
-    #sslServerCert = "/etc/ssl/certs/mail.pem";
-    #sslServerKey = "/etc/ssl/private/mail.key";
-    #extraConfig = ''
-    #  ssl = required
-    #'';
+    sslServerCert = "/etc/ssl/certs/mail.pem";
+    sslServerKey = "/etc/ssl/private/mail.key";
+    extraConfig = ''
+      ssl = required
+    '';
+    extraConfig = ''
+      namespace inbox {
+        mailbox trash {
+          auto = create
+          special_use = \Trash
+        }
+        mailbox sent {
+          auto = create
+          special_use = \Sent
+        }
+        mailbox junk {
+          auto = create
+          special_use = \Junk
+        }
+      }
+
+      service auth {
+        unix_listener /var/spool/postfix/private/auth {
+          mode = 0660
+          user = postfix
+          group = postfix
+        }
+      }
+    '';
   };
 }
