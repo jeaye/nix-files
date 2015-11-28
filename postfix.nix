10c0d4eb284e145a631f542cd7b12f21c58c1edc
jeaye <contact@jeaye.com>
2015-11-28 15:23:22 +0800

    Add some tls/sasl bits for dovecot

--- postfix.nix
+++ postfix.nix
@@ -47,6 +47,25 @@
     extraConfig = ''
       home_mailbox = Maildir/
       virtual_alias_domains = jeaye.com fu-er.com furthington.com arrownext.com
+
+      # Security
+      smtpd_use_tls=yes
+      smtpd_tls_cert_file=/etc/ssl/certs/mail.pem
+      smtpd_tls_key_file=/etc/ssl/private/mail.key
+      smtpd_tls_session_cache_database = btree:$\{data_directory}/smtpd_scache
+      smtp_tls_session_cache_database = btree:$\{data_directory}/smtp_scache
+      smtpd_tls_security_level=may
+      smtpd_tls_protocols = !SSLv2, !SSLv3
+
+      smtpd_recipient_restrictions =
+          permit_sasl_authenticated
+          permit_mynetworks
+          reject_unauth_destination
+
+      smtpd_sasl_auth_enable = yes
+      smtpd_sasl_type = dovecot
+      smtpd_sasl_path = private/auth
+      smtpd_sasl_authenticated_header = yes
     '';
   };
 }
