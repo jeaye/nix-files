8d6001864cc2ea499342ad960dbc9854893c602e
jeaye <contact@jeaye.com>
2015-11-28 21:22:19 +0800

    Disable  sudo

--- user.nix
+++ user.nix
@@ -1,7 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  security.sudo.wheelNeedsPassword = true;
+  security.sudo = {
+    enable = false;
+    wheelNeedsPassword = true;
+  };
   security.pam.loginLimits = [
     { domain = "*"; item = "nproc"; type = "soft"; value = "500"; }
     { domain = "*"; item = "nproc"; type = "hard"; value = "500"; }
