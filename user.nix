87de3fc05dbfb80c7fe98a11609653a5a0973793
jeaye <contact@jeaye.com>
2015-11-28 22:38:32 +0800

    Limit maxlogins

--- user.nix
+++ user.nix
@@ -9,6 +9,7 @@
   security.pam.loginLimits = [
     { domain = "*"; item = "nproc"; type = "soft"; value = "500"; }
     { domain = "*"; item = "nproc"; type = "hard"; value = "500"; }
+    { domain = "*"; item = "maxlogins"; type = "hard"; value = "3"; }
   ];
 
   users.extraUsers.jeaye = {
