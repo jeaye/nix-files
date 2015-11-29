acacc049208867ae37bcdcc3b4aabe8a3aa26bf9
jeaye <contact@jeaye.com>
2015-11-29 12:44:20 +0800

    Refactor into environment.nix

---
+++ environment.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  time.timeZone = "America/Los_Angeles";
+  services.ntp.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    wget
+    elinks
+    unzip
+    git
+    htop
+    bashCompletion
+  ];
+
+  programs.bash.enableCompletion = true;
+
+  nixpkgs.config = {
+    packageOverrides = pkgs: rec {
+      fiche = pkgs.callPackage ./pkg/fiche.nix { };
+      gnupg = pkgs.gnupg.override { x11Support = false; };
+    };
+  };
+
+  # Don't bring in any X dependencies
+  environment.noXlibs = true;
+  fonts.fontconfig.enable = false;
+}
