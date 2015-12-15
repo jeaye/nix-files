5f30f1bea6ae878c12a09b16c0facbe0f037de43
jeaye <contact@jeaye.com>
2015-12-15 17:56:39 +0800

    Add a nofile pam limit

--- system/user.nix
+++ system/user.nix
@@ -12,6 +12,7 @@
     { domain = "*"; item = "nproc"; type = "soft"; value = "500"; }
     { domain = "*"; item = "nproc"; type = "hard"; value = "500"; }
     { domain = "*"; item = "maxlogins"; type = "hard"; value = "3"; }
+    { domain = "*"; item = "nofile"; type = "hard"; value = "512"; }
   ];
 
   users.users.jeaye =
