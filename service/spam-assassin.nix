27698eef6b8cf65aa9a3c4ad45ea399966775813
jeaye <contact@jeaye.com>
2015-12-17 15:18:16 +0800

    Allow more lenient writing to the postfix queue

--- service/spam-assassin.nix
+++ service/spam-assassin.nix
@@ -118,7 +118,7 @@ in
       cp -n ${pkgs.spamassassin}/share/spamassassin/* /etc/spamassassin/
       rm -f /etc/spamassassin/local.cf
       ln -s ${localcf} /etc/spamassassin/local.cf
-      #chmod -R 0774 /var/postfix/queue
+      chmod -R 0776 /var/postfix/queue
     '';
   } else {};
 
