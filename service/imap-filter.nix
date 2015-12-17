c24a8263369e48f2ba005012aa9cc939f463d75a
jeaye <contact@jeaye.com>
2015-12-17 21:22:48 +0800

    Setup proper perms for imap-filter

--- service/imap-filter.nix
+++ service/imap-filter.nix
@@ -97,7 +97,9 @@
       deps = [];
       text =
       ''
-        mkdir -p /home/jeaye/.secret /home/jeaye/.imapfilter
+        mkdir -p /home/jeaye/{.secret,.imapfilter}
+        chown -R jeaye:users /home/jeaye/{.secret,.imapfilter}
+        chmod -R 0700 /home/jeaye/.secret
         ln -sf /etc/user/jeaye/imap-filter/config.lua /home/jeaye/.imapfilter/
       '';
     };
