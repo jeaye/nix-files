03d208b05143c850aa4ded6e80aebe4ecfdcb1a4
jeaye <contact@jeaye.com>
2015-12-17 21:48:39 +0800

    Clean up imap-filter config

--- service/data/imap-filter-config.lua
+++ service/data/imap-filter-config.lua
@@ -11,25 +11,19 @@ function main()
   account.INBOX:check_status()
 
   -- Get all mail from INBOX
-  mails = account.INBOX:select_all()
-
-  -- Move mailing lists from INBOX to correct folders
+  local mails = account.INBOX:select_all()
   move_mailing_lists(account, mails)
 
-  -- Delete some trash
+  -- Ignore some senders
   delete_mail_from(account, mails, "foo@spam.com");
 
   -- Get all mail from trash
-  mails = account['Trash']:select_all()
-
-  -- Move mailing lists from trash to correct folders
-  move_mailing_lists(account, mails)
+  local trash = account['Trash']:select_all()
+  move_mailing_lists(account, trash)
 
   -- Get all mail from spam
-  mails = account['Spam']:select_all()
-
-  -- Move mailing lists from spam to correct folders
-  move_mailing_lists(account, mails)
+  local spam = account['Spam']:select_all()
+  move_mailing_lists(account, spam)
 end
 
 function move_mailing_lists(account, mails)
@@ -63,7 +57,6 @@ function delete_mail_if_subject_contains(account, mails, subject)
   filtered:delete_messages()
 end
 
--- Utility function to get IMAP password from file
 function get_imap_password(file)
   local home = os.getenv("HOME")
   local file = home .. "/" .. file
