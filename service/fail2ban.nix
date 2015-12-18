aae8c0d5f749d9696f62691a67a9552a52cf603e
jeaye <contact@jeaye.com>
2015-12-18 18:36:19 +0800

    Clean up some action names

--- service/fail2ban.nix
+++ service/fail2ban.nix
@@ -13,20 +13,20 @@
     ''
       filter = sshd
       maxretry = 5
-      action   = iptables[name=ssh, port=ssh, protocol=tcp]
+      action   = iptables[name=SSH, port=ssh, protocol=tcp]
       enabled  = true
     '';
     jails.sshd-ddos =
     ''
       filter = sshd-ddos
       maxretry = 2
-      action   = iptables[name=ssh, port=ssh, protocol=tcp]
+      action   = iptables[name=SSH, port=ssh, protocol=tcp]
       enabled  = true
     '';
     jails.port-scan =
     ''
       filter   = port-scan
-      action   = iptables-allports[name=portscan]
+      action   = iptables-allports[name=port-scan]
       maxretry = 2
       bantime  = 7200
       enabled  = true
