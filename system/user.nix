66e0d7820f4d501fd83f6bfa400f6b890e32e9f7
jeaye <contact@jeaye.com>
2015-12-20 21:46:11 +0800

    Use a stricter nproc pam limit

--- system/user.nix
+++ system/user.nix
@@ -9,8 +9,8 @@
 
   security.pam.loginLimits =
   [
-    { domain = "*"; item = "nproc"; type = "soft"; value = "500"; }
-    { domain = "*"; item = "nproc"; type = "hard"; value = "500"; }
+    { domain = "*"; item = "nproc"; type = "soft"; value = "256"; }
+    { domain = "*"; item = "nproc"; type = "hard"; value = "256"; }
     { domain = "*"; item = "maxlogins"; type = "hard"; value = "3"; }
     { domain = "*"; item = "nofile"; type = "hard"; value = "512"; }
   ];
