a3b50f1ba82749b068a9d760c1249e943a21541c
jeaye <contact@jeaye.com>
2015-12-20 11:45:00 +0800

    Split up acme domains

--- service/acme.nix
+++ service/acme.nix
@@ -21,20 +21,51 @@
         {
           "www.pastespace.org" = null;
           "mail.pastespace.org" = null;
-
-          "jeaye.com" = null;
-          "www.jeaye.com" = null;
-          "mail.jeaye.com" = null;
-
-          "fu-er.com" = null;
+        };
+        email = "contact@jeaye.com";
+        plugins = [ "chain.pem" "key.pem" "cert.pem" ];
+        postRun = "systemctl restart httpd.service";
+      };
+      "mail.jeaye.com" =
+      {
+        webroot = "/home/http/mail.jeaye.com";
+        email = "contact@jeaye.com";
+        plugins = [ "chain.pem" "key.pem" "cert.pem" ];
+        postRun = "systemctl restart httpd.service";
+      };
+      "fu-er.com" =
+      {
+        webroot = "/home/http/fu-er.com";
+        extraDomains =
+        {
           "www.fu-er.com" = null;
           "mail.fu-er.com" = null;
-
-          "penelope-art.com" = null;
+        };
+        email = "contact@jeaye.com";
+        plugins = [ "chain.pem" "key.pem" "cert.pem" ];
+        postRun = "systemctl restart httpd.service";
+      };
+      "penelope-art.com" =
+      {
+        webroot = "/home/http/penelope-art.com";
+        extraDomains =
+        {
           "www.penelope-art.com" = null;
           "mail.penelope-art.com" = null;
 
           "penny-art.com" = null;
+          "www.penny-art.com" = null;
+          "mail.penny-art.com" = null;
+        };
+        email = "contact@jeaye.com";
+        plugins = [ "chain.pem" "key.pem" "cert.pem" ];
+        postRun = "systemctl restart httpd.service";
+      };
+      "penny-art.com" =
+      {
+        webroot = "/home/http/penny-art.com";
+        extraDomains =
+        {
           "www.penny-art.com" = null;
           "mail.penny-art.com" = null;
         };
