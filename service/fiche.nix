68f15f014b0f4517060ea16fb78300aa31c75069
jeaye <contact@jeaye.com>
2016-01-09 17:02:29 +0800

    Revert "Enable private networking for fiche container"
    This reverts commit 321541a0081ae5a5aebbd3bddc62e4f309f02b7b.

--- service/fiche.nix
+++ service/fiche.nix
@@ -9,14 +9,14 @@
     };
   };
 
-  containers.fiche =
+  containers.database =
   {
-    privateNetwork = true;
-    hostAddress = "192.168.100.10";
-    localAddress = "192.168.100.11";
-
     config = { config, pkgs, ... }:
     {
+      privateNetwork = true;
+      hostAddress = "192.168.100.10";
+      localAddress = "192.168.100.11";
+
       systemd.services.fiche =
       {
         wantedBy = [ "multi-user.target" ];
