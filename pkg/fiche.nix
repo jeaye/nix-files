c83b1dfda4b3e601973c74ca12c9762ade67076d
jeaye <contact@jeaye.com>
2016-01-23 22:17:08 +0800

    Update fiche

--- pkg/fiche.nix
+++ pkg/fiche.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation rec
   {
     owner = "solusipse";
     repo = "fiche";
-    rev = "8f3e23d3de66aa894f78417523cb6aeb208b6d60";
+    rev = "a5c137c06d20e96fb97e946378437ec198299287";
     sha256 = "1371gafnysddg7zip2rdrp3wd8n4xc6ddvkcqfg4b4jc6i2hxxax";
   };
   buildInputs = [];
