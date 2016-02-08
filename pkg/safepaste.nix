e033bbdd55f822d6ba4e529f9346c59ab588791c
jeaye <contact@jeaye.com>
2016-02-08 12:14:44 +0800

    Bring in boot from unstable

--- pkg/safepaste.nix
+++ pkg/safepaste.nix
@@ -1,20 +1,28 @@
 { stdenv, pkgs, fetchFromGitHub }:
 
-stdenv.mkDerivation rec
+let pkgsUnstable = import
+(
+  fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz
+)
+{ };
+in
 {
-  name = "safepaste";
-  src = pkgs.fetchFromGitHub
+  stdenv.mkDerivation rec
   {
-    owner = "jeaye";
-    repo = "safepaste";
-    rev = "cbaf3939970cf2e5964946dcad001330d4ee5324";
-    sha256 = "0kvywffrh4723051ndyb5zzb81bcc5x5q2qpkp26m9g65q8xdf82";
-  };
-  buildInputs = [ boot nodejs ];
-  installPhase =
-  ''
-    mkdir -p $out/bin;
-    boot build
-    install -m 0644 target/safepaste.jar $out/bin/
-  '';
+    name = "safepaste";
+    src = pkgs.fetchFromGitHub
+    {
+      owner = "jeaye";
+      repo = "safepaste";
+      rev = "cbaf3939970cf2e5964946dcad001330d4ee5324";
+      sha256 = "0kvywffrh4723051ndyb5zzb81bcc5x5q2qpkp26m9g65q8xdf82";
+    };
+    buildInputs = [ pkgsUnstable.boot pkgs.nodejs ];
+    installPhase =
+    ''
+      mkdir -p $out/bin;
+      boot build
+      install -m 0644 target/safepaste.jar $out/bin/
+    '';
+  }
 }
