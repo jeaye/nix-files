16deb637650da52f1ba35238d6a39db722fac29f
jeaye <contact@jeaye.com>
2016-02-08 12:19:55 +0800

    Correct safepaste sha256

--- pkg/safepaste.nix
+++ pkg/safepaste.nix
@@ -1,12 +1,11 @@
 { stdenv, pkgs, fetchFromGitHub }:
 
-let pkgsUnstable = import
-(
-  fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz
-)
-{ };
+let
+  pkgsUnstable = import
+  (
+    fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz
+  ) { };
 in
-{
   stdenv.mkDerivation rec
   {
     name = "safepaste";
@@ -15,7 +14,7 @@ in
       owner = "jeaye";
       repo = "safepaste";
       rev = "cbaf3939970cf2e5964946dcad001330d4ee5324";
-      sha256 = "0kvywffrh4723051ndyb5zzb81bcc5x5q2qpkp26m9g65q8xdf82";
+      sha256 = "1ax8pnaa0l7cr2sya50gjvhfr4j401vw70q973ms5mla95l6zqvh";
     };
     buildInputs = [ pkgsUnstable.boot pkgs.nodejs ];
     installPhase =
@@ -25,4 +24,3 @@ in
       install -m 0644 target/safepaste.jar $out/bin/
     '';
   }
-}
