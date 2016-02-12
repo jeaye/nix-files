d0c1a972a1c5a30f7cd184938bdf3ccdbfca6bee
jeaye <contact@jeaye.com>
2016-02-12 18:14:06 +0800

    Update safepaste revision

--- pkg/safepaste.nix
+++ pkg/safepaste.nix
@@ -13,7 +13,7 @@ in
     {
       url = "https://github.com/jeaye/safepaste.git";
       deepClone = true;
-      rev = " d6e97b3eb194170cf3873ccd1c4869cd3180dd06";
+      rev = "d6e97b3eb194170cf3873ccd1c4869cd3180dd06";
       sha256 = "190hbmbi3b8f7scc4i3c6wn71wz1x18aa83rqkds44r8mky5h4jn";
     };
     __noChroot = true;
