b1cd1bb11594986bb5689afc74230a158d9b215d
jeaye <contact@jeaye.com>
2016-02-27 22:46:38 +0800

    Tweak upload.jeaye.com/tmp cleanup cron

--- service/upload.jeaye.com-tmp.nix
+++ service/upload.jeaye.com-tmp.nix
@@ -4,6 +4,6 @@
   # Clean up old files
   services.cron.systemCronJobs =
   [
-    "0 0 * * * http ${pkgs.findutils}/bin/find /etc/user/http/upload.jeaye.com/tmp/* -mtime +2 -type d -exec rm -r {} \\;"
+    "0 0 * * * http ${pkgs.findutils}/bin/find /etc/user/http/upload.jeaye.com/tmp -mtime +2 -type f -exec rm -vf {} \\;"
   ];
 }
