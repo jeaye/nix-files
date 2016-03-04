5f7afe91b3cabff8d92b8ec94943014d1081d4f8
jeaye <contact@jeaye.com>
2016-03-03 20:11:25 -0800

    Bring in jeayer nix expression

---
+++ user/jeaye.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  users.users.jeaye =
+  {
+    isNormalUser = true;
+    home = "/etc/user/jeaye";
+    extraGroups = [ "wheel" ];
+  };
+
+  system.activationScripts =
+  {
+    jeaye-home =
+    {
+      deps = [];
+      text =
+      ''
+        if [ ! -d /etc/user/jeaye/.vim ];
+        then
+          git clone --recursive https://github.com/jeaye/vimrc.git /etc/user/jeaye/.vim
+          ln -sf /etc/user/jeaye/.vim/vimrc /etc/user/jeaye/.vimrc
+        fi
+        chgrp -R jeaye /etc/user/jeaye/.vim
+      '';
+    };
+  };
+}
