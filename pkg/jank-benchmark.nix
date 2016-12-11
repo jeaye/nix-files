40bc1aba404cef1380ffc0b45e8ba18b41520414
jeaye <contact@jeaye.com>
2016-12-11 13:08:55 -0800

    Update jank-benchmark sha256

--- pkg/jank-benchmark.nix
+++ pkg/jank-benchmark.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec
     url = "https://github.com/jeaye/jank-benchmark.git";
     deepClone = true;
     rev = "8786b1e788224263cc5f6ce8aab378ff6d911998";
-    sha256 = "13gb6dv72jn5bgw04bqi9cx77dqbqrr5ylx8qjhyb4l4p0x93bqy";
+    sha256 = "1g36f1pdfdjw0gj96y3n9nszqh5ds20ccsf1znw6199s1awnawws";
   };
   buildInputs = [ pkgs.leiningen ];
   buildPhase =
