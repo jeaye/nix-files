2e9cc711bca13b20604235d37f04a68f8adf6dfc
jeaye <contact@jeaye.com>
2016-12-11 13:03:08 -0800

    Give jank-benchmark a home

--- service/jank-benchmark.nix
+++ service/jank-benchmark.nix
@@ -11,7 +11,38 @@
   {
     packageOverrides = pkgs: rec
     {
-      nix-benchmark = pkgs.callPackage ../pkg/nix-benchmark.nix { };
+      jank-benchmark = pkgs.callPackage ../pkg/jank-benchmark.nix { };
+    };
+  };
+
+  environment.etc."user/jank-benchmark/run-server" =
+  {
+    text =
+    ''
+      #!/run/current-system/sw/bin/bash
+      set -eu
+      ${pkgs.openjdk}/bin/java -jar ${pkgs.jank-benchmark}/bin/jank-benchmark.jar
+    '';
+    mode = "0775";
+  };
+
+  users.users.jank-benchmark =
+  {
+    isNormalUser = false;
+    home = "/etc/user/jank-benchmark";
+    createHome = true;
+  };
+  environment.etc."user/jank-benchmark/paste/.manage-directory".text = "";
+
+  system.activationScripts =
+  {
+    jank-benchmark-home =
+    {
+      deps = [];
+      text =
+      ''
+        chown -R jank-benchmark:users /etc/user/jank-benchmark
+      '';
     };
   };
 }
