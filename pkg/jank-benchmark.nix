c638e1a0b6c5b8928ac3469a9307c1bb9dd76bb6
jeaye <contact@jeaye.com>
2016-12-17 22:36:27 -0800

    Update jank-benchmark revision

--- pkg/jank-benchmark.nix
+++ pkg/jank-benchmark.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation rec
   {
     url = "https://github.com/jeaye/jank-benchmark.git";
     deepClone = true;
-    rev = "aa651f575b74db314f0b78cfc2face0b0b8d8de1";
+    rev = "cd31bbfa1f90d7a17abacc0ef06b8657f82f200c";
     sha256 = "07d7x293zv4f5frx7gh010g4ldj3y8ak6zvqnc0jbx8gpzvz6635";
   };
   buildInputs = [ pkgs.leiningen ];
