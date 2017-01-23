de9b7908938c1bb818fc668f18d6109a18c5bcf6
jeaye <contact@jeaye.com>
2017-01-22 20:48:28 -0800

    Fill in some server settings for radicale

--- service/radicale.nix
+++ service/radicale.nix
@@ -1,7 +1,29 @@
 { config, pkgs, ... }:
 
 {
-  services.radicale.enable = true;
+  services.radicale = {
+    enable = true;
+    config =
+    ''
+      [server]
+      ## SSL
+      ssl = True
+      certificate = /var/lib/acme/pastespace.org/cert.pem
+      key = /var/lib/acme/pastespace.org/key.pem
+
+      [auth]
+      # Authentication method
+      # Value: None | htpasswd | IMAP | LDAP | PAM | courier | http | remote_user | custom
+      type = PAM
+
+      imap_hostname = localhost
+      imap_port = 143
+      imap_ssl = True
+
+      [git]
+      committer = Radicale <radicale@jeaye.com>
+    '';
+  };
 
   networking.firewall =
   {
