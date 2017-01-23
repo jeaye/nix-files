9371b9fb3c0eaba34a291684a49c22e685eec239
jeaye <contact@jeaye.com>
2017-01-22 21:00:04 -0800

    Unindent radicale config

--- service/radicale.nix
+++ service/radicale.nix
@@ -5,23 +5,26 @@
     enable = true;
     config =
     ''
-      [server]
-      ## SSL
-      ssl = True
-      certificate = /var/lib/acme/pastespace.org/cert.pem
-      key = /var/lib/acme/pastespace.org/key.pem
+[server]
+## SSL
+ssl = True
+certificate = /var/lib/acme/pastespace.org/cert.pem
+key = /var/lib/acme/pastespace.org/key.pem
 
-      [auth]
-      # Authentication method
-      # Value: None | htpasswd | IMAP | LDAP | PAM | courier | http | remote_user | custom
-      type = PAM
+[auth]
+# Authentication method
+# Value: None | htpasswd | IMAP | LDAP | PAM | courier | http | remote_user | custom
+type = PAM
 
-      imap_hostname = localhost
-      imap_port = 143
-      imap_ssl = True
+imap_hostname = localhost
+imap_port = 143
+imap_ssl = True
 
-      [git]
-      committer = Radicale <radicale@jeaye.com>
+[git]
+committer = Radicale <radicale@jeaye.com>
+
+[logging]
+debug = True
     '';
   };
 
