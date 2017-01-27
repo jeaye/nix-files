4dc2126b9f09579ab2e2b766987639cd5557f43d
jeaye <contact@jeaye.com>
2017-01-26 21:00:32 -0800

    Clean up some radicale config

--- service/radicale.nix
+++ service/radicale.nix
@@ -6,7 +6,9 @@
     config =
     ''
 [server]
+hosts = 0.0.0.0:5232
 base_prefix = /
+can_skip_base_prefix = True
 
 [auth]
 type = None
@@ -18,7 +20,7 @@ type = owner_only
 committer = Radicale <radicale@jeaye.com>
 
 [logging]
-debug = False
+debug = True
     '';
   };
 
@@ -26,44 +28,26 @@ debug = False
   {
     "radicale/logging".text =
     ''
-# Loggers, handlers and formatters keys
-
 [loggers]
-# Loggers names, main configuration slots
 keys = root
 
 [handlers]
-# Logging handlers, defining logging output methods
 keys = console
 
 [formatters]
-# Logging formatters
-keys = simple,full
-
-# Loggers
+keys = full
 
 [logger_root]
-# Root logger
 level = DEBUG
 handlers = console
 
-# Handlers
-
 [handler_console]
-# Console handler
 class = StreamHandler
-level = INFO
+level = DEBUG
 args = (sys.stdout,)
-formatter = simple
-
-# Formatters
-
-[formatter_simple]
-# Simple output format
-format = %(message)s
+formatter = full
 
 [formatter_full]
-# Full output format
 format = %(asctime)s - %(levelname)s: %(message)s
     '';
   };
