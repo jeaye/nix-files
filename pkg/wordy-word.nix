c6c6a6a70604679c7a9bf004929cb1bf3d29825a
jeaye <contact@jeaye.com>
2017-02-15 12:28:50 -0800

    Require wget for build

--- pkg/wordy-word.nix
+++ pkg/wordy-word.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec
     rev = "dca1106f6bab7f44ff44c74a87b7659d72bec55a";
     sha256 = "076gbpjwh0xdv15mn3gcpkfwj1ml6vkava4m5k341xmdz06c01wa";
   };
-  buildInputs = [ pkgs.leiningen ];
+  buildInputs = [ pkgs.leiningen, pkgs.wget ];
   buildPhase =
   ''
     # For leiningen
