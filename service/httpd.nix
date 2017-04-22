091f00e2e2a4822f7cc80da253c619ce44748224
jeaye <contact@jeaye.com>
2017-04-22 13:25:21 -0700

    Revert "Refactor certs to share more nicely"
    This reverts commit ef4f7975fa8cb3ab5834f069da6f4744bcddbe91.

--- service/httpd.nix
+++ service/httpd.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 let
-  ssl_info = domain: cert_domain:
+  ssl_info = domain:
   ''
     <Directory /etc/user/http/${domain}/.well-known>
       AllowOverride None
@@ -10,9 +10,9 @@ let
     </Directory>
     Alias /.well-known/ /etc/user/http/${domain}/.well-known/
 
-    SSLCertificateKeyFile /var/lib/acme/${cert_domain}/key.pem
-    SSLCertificateChainFile /var/lib/acme/${cert_domain}/chain.pem
-    SSLCertificateFile /var/lib/acme/${cert_domain}/cert.pem
+    SSLCertificateKeyFile /var/lib/acme/${domain}/key.pem
+    SSLCertificateChainFile /var/lib/acme/${domain}/chain.pem
+    SSLCertificateFile /var/lib/acme/${domain}/cert.pem
     SSLProtocol All -SSLv2 -SSLv3
     SSLCipherSuite HIGH:!aNULL:!MD5:!EXP
     SSLHonorCipherOrder on
@@ -37,6 +37,8 @@ in
     extraModules = [ "proxy" "proxy_http" ];
 
     # TODO: Add proxy helper fn
+    # TODO: Bring blog.jeaye.com into jeaye.com cert
+    # TODO: Allow reading cert from other domain in helper fns
     virtualHosts =
     [
       {
@@ -68,28 +70,7 @@ in
           ProxyPass /.well-known !
           ProxyPass /calendar http://localhost:5232/
           ProxyPassReverse /calendar http://localhost:5232/
-        '' + (defaults "pastespace.org" "pastespace.org");
-        enableSSL = true;
-      }
-      {
-        hostName = "webmail.pastespace.org";
-        documentRoot = "/etc/user/http/webmail.pastespace.org";
-        extraConfig =
-        ''
-          # XXX: Requires manual creation using htpasswd
-          <Directory /etc/user/http/webmail.pastespace.org>
-            DirectoryIndex index.php
-            Options +Indexes +FollowSymLinks +ExecCGI
-            AllowOverride All
-            Order deny,allow
-            Allow from all
-            Require all granted
-          </Directory>
-          <Directory /etc/user/http/webmail.pastespace.org/data>
-            Options -Indexes
-            Deny from all
-          </Directory>
-        '' + (defaults "webmail.pastespace.org" "pastespace.org");
+        '' + (defaults "pastespace.org");
         enableSSL = true;
       }
       {
@@ -111,7 +92,7 @@ in
           ProxyPass /.well-known !
           ProxyPass / http://localhost:3000/
           ProxyPassReverse / http://localhost:3000/
-        '' + (defaults "safepaste.org" "safepaste.org");
+        '' + (defaults "safepaste.org");
         enableSSL = true;
       }
       {
@@ -123,7 +104,7 @@ in
         documentRoot = "/etc/user/http/upload.jeaye.com";
         extraConfig =
         ''
-        '' + (defaults "upload.jeaye.com" "jeaye.com");
+        '' + (defaults "upload.jeaye.com");
         enableSSL = true;
       }
       {
@@ -143,7 +124,7 @@ in
           ProxyPass / https://jeaye.github.io/jeaye.com/
           ProxyPassReverse / https://jeaye.github.io/jeaye.com/
           ProxyPassReverse / http://jeaye.github.io/jeaye.com/
-        '' + (defaults "jeaye.com" "jeaye.com");
+        '' + (defaults "jeaye.com");
         enableSSL = true;
       }
       {
@@ -161,7 +142,7 @@ in
           ProxyPass / https://jeaye.github.io/blog.jeaye.com/
           ProxyPassReverse / https://jeaye.github.io/blog.jeaye.com/
           ProxyPassReverse / http://jeaye.github.io/blog.jeaye.com/
-        '' + (defaults "blog.jeaye.com" "jeaye.com");
+        '' + (defaults "blog.jeaye.com");
         enableSSL = true;
       }
       {
@@ -207,7 +188,7 @@ in
         documentRoot = "/etc/user/http/fu-er.com";
         extraConfig =
         ''
-        '' + (defaults "fu-er.com" "fu-er.com");
+        '' + (defaults "fu-er.com");
         enableSSL = true;
       }
       {
@@ -224,7 +205,7 @@ in
         extraConfig =
         ''
           DirectoryIndex resume.pdf
-        '' + (defaults "penelope-art.com" "penelope-art.com");
+        '' + (defaults "penelope-art.com");
         enableSSL = true;
       }
       {
@@ -240,7 +221,7 @@ in
         globalRedirect = "https://penny.artstation.com/";
         extraConfig =
         ''
-        '' + (defaults "penny-art.com" "penny-art.com");
+        '' + (defaults "penny-art.com");
         enableSSL = true;
       }
     ];
