91f5dad83774388ca39515c383c30be364c84367
jeaye <contact@jeaye.com>
2017-04-23 18:51:06 -0700

    Add curl and wget to rainloop script

--- service/rainloop.nix
+++ service/rainloop.nix
@@ -6,7 +6,7 @@
     rainloop =
     {
       deps = [];
-      path = [pkgs.gnupg pkgs.rsync pkgs.unzip];
+      path = [pkgs.curl pkgs.wget pkgs.gnupg pkgs.rsync pkgs.unzip];
       text = lib.readFile ./data/upgrade-rainloop;
     };
   };
