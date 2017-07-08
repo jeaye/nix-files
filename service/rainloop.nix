eec03df933d4ccb7c1cc4bd5c31d6d6dcc45cce1
jeaye <contact@jeaye.com>
2017-07-08 15:21:30 -0700

    Switch rainloop upgrade to a cron job

--- service/rainloop.nix
+++ service/rainloop.nix
@@ -1,16 +1,21 @@
 { config, pkgs, lib, ... }:
 
 {
-  system.activationScripts =
+  environment.etc =
   {
-    rainloop =
+    "upgrade-rainloop" =
     {
-      deps = [];
       text =
       ''
         export PATH=${pkgs.curl}/bin:${pkgs.wget}/bin:${pkgs.gnupg}/bin:${pkgs.rsync}/bin:${pkgs.unzip}/bin:$PATH
         ${lib.readFile ./data/upgrade-rainloop}
       '';
+      mode = "0774";
     };
   };
+
+  services.cron.systemCronJobs =
+  [
+    "@daily root /etc/upgrade-rainloop"
+  ];
 }
