c6c4dc8264537217d69d717e7bf4d37888005937
jeaye <contact@jeaye.com>
2017-08-26 22:12:04 -0700

    Add virtual http host for container

--- container/nextcloud.nix
+++ container/nextcloud.nix
@@ -1,84 +1,114 @@
 { config, pkgs, ... }:
 
-# TODO: Private network
+# TODO: Install nextcloud
 # TODO: Update script, like rainloop has
 
-containers.nextcloud =
+let
+  hostAddr = "192.168.255.1";
+  localAddr = "192.168.254.1";
+in
 {
-  autoStart = true;
-  config = { config, pkgs, ... }:
+  networking.extraHosts = "${hostAddr} nextcloud";
+  containers.nextcloud =
   {
-    services.httpd =
+    autoStart = true;
+    privateNetwork = true;
+    hostAddress = hostAddr;
+    localAddress = localAddr;
+
+    config = { config, pkgs, ... }:
     {
-      enable = true;
-      user = "http";
-      adminAddr = "contact@jeaye.com";
+      services.httpd =
+      {
+        enable = true;
+        user = "http";
+        adminAddr = "contact@jeaye.com";
 
-      logPerVirtualHost = true;
+        logPerVirtualHost = true;
 
-      extraModules =
-      [
-        "rewrite" "headers" "env" "dir" "mime"
-        { name = "php7"; path = "${pkgs.php}/modules/libphp7.so"; }
-      ];
+        extraModules =
+        [
+          "rewrite" "headers" "env" "dir" "mime"
+          { name = "php7"; path = "${pkgs.php}/modules/libphp7.so"; }
+        ];
 
-      virtualHosts =
-      [
-        #{
-        #  hostName = "cloud.pastespace.org";
-        #  globalRedirect = "https://cloud.pastespace.org/";
-        #  enableSSL = false;
-        #}
-        {
-          hostName = "cloud.pastespace.org";
-          documentRoot = "/etc/user/http/cloud.pastespace.org";
-          extraConfig =
-          ''
-            <Directory /etc/user/http/cloud.pastespace.org>
-              Options +FollowSymlinks
-              AllowOverride All
+        virtualHosts =
+        [
+          {
+            hostName = "cloud.pastespace.org";
+            documentRoot = "/etc/user/http/cloud.pastespace.org";
+            extraConfig =
+            ''
+              <Directory /etc/user/http/cloud.pastespace.org>
+                Options +FollowSymlinks
+                AllowOverride All
 
-              <IfModule mod_dav.c>
-                Dav off
-              </IfModule>
+                <IfModule mod_dav.c>
+                  Dav off
+                </IfModule>
 
-              SetEnv HOME /etc/user/http/cloud.pastespace.org
-              SetEnv HTTP_HOME /etc/user/http/cloud.pastespace.org
-            </Directory>
-          '';
-          #enableSSL = true;
-        }
-      ];
+                SetEnv HOME /etc/user/http/cloud.pastespace.org
+                SetEnv HTTP_HOME /etc/user/http/cloud.pastespace.org
+              </Directory>
+            '';
+          }
+        ];
 
-      extraConfig = # TODO: Share with host
-      ''
-        AddDefaultCharset UTF-8
-        AddCharset UTF-8 .html .htm .txt
+        extraConfig = # TODO: Share with host
+        ''
+          AddDefaultCharset UTF-8
+          AddCharset UTF-8 .html .htm .txt
 
-        ServerTokens Prod
-        ServerSignature Off
-        TraceEnable off
-      '';
-    };
+          ServerTokens Prod
+          ServerSignature Off
+          TraceEnable off
+        '';
+      };
 
-    environment.etc =
-    {
-      "user/http/cloud.pastespace.org/.well-known/.manage-directory".text = "";
-    };
+      users.users.http =
+      {
+        isNormalUser = false;
+        home = "/etc/user/http";
+      };
 
-    users.users.http =
-    {
-      isNormalUser = false;
-      home = "/etc/user/http";
-    };
-
-    networking.firewall =
-    {
-      allowedTCPPorts =
-      [
-        80 # http
-        443 # https
-      ];
+      networking.firewall =
+      {
+        allowedTCPPorts =
+        [
+          80 # http
+          443 # https
+        ];
+      };
     };
   };
-};
+
+  services.httpd =
+  {
+    virtualHosts =
+    [
+      #{
+      #  hostName = "cloud.pastespace.org";
+      #  globalRedirect = "https://cloud.pastespace.org/";
+      #  enableSSL = false;
+      #}
+      {
+        hostName = "cloud.pastespace.org";
+        documentRoot = "/etc/user/http/cloud.pastespace.org";
+        extraConfig =
+        ''
+          #SSLProxyEngine On
+          ProxyPreserveHost Off
+          ProxyPass /.well-known !
+          ProxyPass / http://nextcloud/
+          ProxyPassReverse / http://nextcloud/
+        ''; #TODO + (defaults "cloud.pastespace.org" "cloud.pastespace.org");
+        enableSSL = false;
+      }
+    ];
+  };
+
+  environment.etc =
+  {
+    "user/http/cloud.pastespace.org/.well-known/.manage-directory".text = "";
+  };
+}
