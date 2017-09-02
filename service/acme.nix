bf3199fc01e25f23a83065cfd27596d0264a9254
jeaye <contact@jeaye.com>
2017-09-02 14:24:15 -0700

    Add cloud.pastespace.org to acme timer

--- service/acme.nix
+++ service/acme.nix
@@ -33,6 +33,7 @@ in
         {
           "www.pastespace.org" = null;
           "mail.pastespace.org" = null;
+          "cloud.pastespace.org:/etc/user/http/cloud.pastespace.org" = null;
           "webmail.pastespace.org:/etc/user/http/webmail.pastespace.org" = null;
         };
         email = global-email;
