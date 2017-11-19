79346fdb50af250aee2c2c1d4ca24ed159cef765
jeaye <contact@jeaye.com>
2017-11-19 11:39:25 -0800

    Add hostname/localhost mapping

--- workstation/system/network.nix
+++ workstation/system/network.nix
@@ -1,12 +1,21 @@
 { config, pkgs, ... }:
 
 {
-  networking =
+  networking = rec
   {
     hostName = "oryx";
 
     enableIPv6 = true;
 
     networkmanager.enable = true;
+
+    hosts =
+    {
+      "127.0.0.1" =
+      [
+        "${hostName}.localdomain"
+        "${hostName}"
+      ];
+    };
   };
 }
