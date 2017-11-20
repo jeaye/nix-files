2bd4c994eee15039eecdb3ab5335b4a595be102d
jeaye <contact@jeaye.com>
2017-11-19 23:09:08 -0800

    Add some more daily programs

--- workstation/user/jeaye/program.nix
+++ workstation/user/jeaye/program.nix
@@ -1,10 +1,20 @@
 { config, pkgs, lib, ... }:
 
 {
+  # TODO: Lock this to user-specific profile
   environment.systemPackages = with pkgs;
   [
     ## Security
     pinentry
     gnupg
+
+    ## Browsing
+    firefox
+
+    ## Calendar
+    thunderbird
+
+    ## Music
+    cmus
   ];
 }
