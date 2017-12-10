53bccb4b6db324f292f9cc92b92eb152ef32c907
jeaye <contact@jeaye.com>
2017-12-09 21:33:04 -0800

    Update pollock hash

--- workstation/system/network/malicious-host.nix
+++ workstation/system/network/malicious-host.nix
@@ -5,7 +5,7 @@ let
   danPollock = pkgs.fetchurl
   {
     url = "http://someonewhocares.org/hosts/zero/hosts";
-    sha256 = "068p0asf0d6h20bjrjlnpibz22ja81hkngx87xyq4yhhq3ibw8nw";
+    sha256 = "165bp7ipxngndyj7csdvsfn3pqi9z2mfd8mkr4pjg304jp3v9iny";
   };
 in
 {
