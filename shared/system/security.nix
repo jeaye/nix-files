41b2998a4ab28945902a57f52239379575429ec2
jeaye <contact@jeaye.com>
2017-12-10 18:57:26 -0800

    Prevent unprivileged users from reading dmesg

--- shared/system/security.nix
+++ shared/system/security.nix
@@ -1,7 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  # Prevent users from read the process info of other users
+  boot.kernel.sysctl =
+  {
+    # Prevent unprivileged users from reading dmesg.
+    "kernel.dmesg_restrict" = 1;
+  };
+
+  # Prevent users from read the process info of other users.
   # TODO: Allow a group id to bypass this
   security.hideProcessInformation = true;
 }
