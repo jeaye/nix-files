88785dd90a22fc543543b144ad82e7c8fb90a7eb
jeaye <contact@jeaye.com>
2017-12-11 22:38:16 -0800

    Mount /tmp with nodev,nosuid,noexec

--- workstation/system/boot.nix
+++ workstation/system/boot.nix
@@ -4,8 +4,33 @@
   boot =
   {
     loader.grub.device = "/dev/sda";
-    tmpOnTmpfs = true;
+    tmpOnTmpfs = false; # Done manually below.
   };
+
+  systemd.mounts =
+  [
+    { # Put /tmp on a protected tmpfs
+      description = "Temporary Directory (/tmp)";
+      what = "tmpfs";
+      where = "/tmp";
+      documentation =
+      [
+        "man:hier(7)"
+        "https://www.freedesktop.org/wiki/Software/systemd/APIFileSystems"
+      ];
+      before = ["local-fs.target" "umount.target"];
+      after = ["swap.target"];
+      conflicts = ["umount.target"];
+      options = "rw,nosuid,nodev,noexec";
+      type = "tmpfs";
+
+      unitConfig =
+      {
+        ConditionPathIsSymbolicLink = "!/tmp";
+        DefaultDependencies = "no";
+      };
+    }
+  ];
 
   services.nixosManual.ttyNumber = 6;
 }
