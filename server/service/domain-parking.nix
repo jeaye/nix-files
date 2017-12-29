833b8270191b604870ca074a0c450c2e0c28d90a
jeaye <contact@jeaye.com>
2017-12-28 18:02:26 -0800

    Generate parked index files from domain list

--- server/service/domain-parking.nix
+++ server/service/domain-parking.nix
@@ -1,22 +1,42 @@
 { config, pkgs, ... }:
 
 let
-  domains = [];
+  domains =
+  [
+    "text-box.org"
+    "safetybox.org"
+    "univps.org"
+    "jank-platform.org"
+    "clojure-atlas.org"
+    "cpp-atlas.org"
+    "history-proxy.org"
+    "helderman.io"
+    "farmware.io"
+    "furthington.com"
+    "anylist.io"
+    "trustcoin.org"
+    "candycapitalist.org"
+    "zenlock.org"
+    "polyvore.io"
+  ];
+  makeVirtualHost = domain:
+  {
+    hostName = domain;
+    serverAliases = ["www." + domain];
+    enableSSL = false;
+    documentRoot = "/etc/user/http/" + domain;
+  };
+  index = builtins.readFile ./data/park-index.html;
+  makeIndexFile = domain:
+  {
+    name = "user/http/" + domain + "/index.html";
+    value =
+    {
+      text = builtins.replaceStrings ["%%domain%%"] domain index;
+    };
+  };
 in
 {
-  services.httpd =
-  {
-    virtualHosts =
-    [
-      {
-        hostName = "bankapp.io";
-        serverAliases =
-        [
-          "www.bankapp.io"
-        ];
-        enableSSL = false;
-        documentRoot = "/etc/user/http/bankapp.io";
-      }
-    ];
-  };
+  services.httpd.virtualHosts = (map makeVirtualHost domains);
+  environment.etc = (map makeIndexFile domains);
 }
